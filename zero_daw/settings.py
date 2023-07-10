73fc692da60d78742817d5deb44d8f2052e61e83
jorge-a11-rgb <jorgeandresalfarocastro@gmail.com>
2023-07-10 15:03:27 -0400

    Faivon.ico error arreglado

--- zero_daw/settings.py
+++ zero_daw/settings.py
@@ -122,7 +122,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
