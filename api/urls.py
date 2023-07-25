807331b24a40cde6e2eb31b10d8d625e08d677f9
jorge-a11-rgb <jorgeandresalfarocastro@gmail.com>
2023-07-25 18:25:18 -0400

    Mejora de busqueda geolocalizacion v1

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('geolocalizacion/', views.geolocalizacion, name='geolocalizacion'),
+     path('geolocalizacion/<int:id_orden>/', views.geolocalizacion, name='geolocalizacion'),
     # Resto de tus URLs
+    path('OrdenListGeolocalizar', views.OrdenListGeolocalizar.as_view(), name='OrdenListGeolocalizar')
 ]
--- api/views.py
+++ api/views.py
@@ -1,47 +1,51 @@
-from django.shortcuts import render
-from django.shortcuts import render, get_object_or_404
-from Registro.models import Orden_reciclaje
 import requests
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.views.generic import ListView
+from Registro.models import Orden_reciclaje
 
-def geolocalizacion(request):
-    if request.method == 'GET':
-        user_id = request.user.id  # Obtener el ID del usuario autenticado
-        
-        # Obtener la instancia del modelo Orden_reciclaje_recolector asociada al usuario
-        orden_reciclaje = Orden_reciclaje.objects.filter(id_user=user_id).first()
 
-        if orden_reciclaje is None:
-            return render(request, 'Api/error_api.html')
-        
-        latitud = float(orden_reciclaje.latitud_posicion_recolector)
-        longitud = float(orden_reciclaje.longitud_posicion_recolector)
-        
-        # Modificar las coordenadas en mapOptions
-        mapOptions = {
-            "center": {"lat": latitud, "lng": longitud},
-            "fullscreenControl": True,
-            "mapTypeControl": False,
-            "streetViewControl": False,
-            "zoom": 12,
-            "zoomControl": True,
-            "maxZoom": 20,
-            "mapId": ""
+def geolocalizacion(request, id_orden):
+    
+    orden_reciclaje = Orden_reciclaje.objects.filter(id_orden=id_orden).first()
+
+    if orden_reciclaje is None:
+        return render(request, 'Api/error_api.html')
+
+    latitud = float(orden_reciclaje.latitud_posicion_recolector)
+    longitud = float(orden_reciclaje.longitud_posicion_recolector)
+
+    mapOptions = {
+        "center": {"lat": latitud, "lng": longitud},
+        "fullscreenControl": True,
+        "mapTypeControl": False,
+        "streetViewControl": False,
+        "zoom": 12,
+        "zoomControl": True,
+        "maxZoom": 20,
+        "mapId": ""
         }
-        
-        # Llamar a la API de geolocalización con las coordenadas proporcionadas
-        response = requests.get(f'https://maps.googleapis.com/maps/api/geocode/json?latlng={latitud},{longitud}&key=AIzaSyAeYeyldDKCcpeYVppGwYKzizbDf_HVnSo')
 
-        if response.status_code == 200:
-            data = response.json()
+    response = requests.get(f'https://maps.googleapis.com/maps/api/geocode/json?latlng={latitud},{longitud}&key=AIzaSyAeYeyldDKCcpeYVppGwYKzizbDf_HVnSo')
 
-            # Procesar los datos de respuesta según tus necesidades
-            # Por ejemplo, puedes extraer la dirección u otros detalles de interés
-
-            return render(request, 'Api/geolocalizacion.html', {'data': data, 'mapOptions': mapOptions})
-        else:
-            data = response.json()
-            return render(request, 'Api/error_api.html', {'data': data})
-        
+    if response.status_code == 200:
+        data = response.json()
+        return render(request, 'Api/geolocalizacion.html', {'data': data, 'mapOptions': mapOptions})
     else:
         data = response.json()
         return render(request, 'Api/error_api.html', {'data': data})
+
+
+
+class OrdenListGeolocalizar(ListView):
+    model = Orden_reciclaje
+    template_name = 'Api/orden_geolocalizar.html'
+
+    def post(self, request, *args, **kwargs):
+        orden_id = request.POST.get('orden_id')
+        return redirect('geolocalizacion', id_orden=orden_id)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['ordenes'] = Orden_reciclaje.objects.all()
+        return context
