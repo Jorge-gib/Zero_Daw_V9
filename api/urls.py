9eebe6ee1924f3d8acd2ab77016549262c0ef34d
jorge-a11-rgb <jorgeandresalfarocastro@gmail.com>
2023-08-06 18:27:19 -0400

    Funcion de ubicar por geolocalizacion a ciudadano fase 2

--- api/urls.py
+++ api/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
      path('geolocalizacion_reserva_ciudadano/<int:id_orden>/', views.geolocalizacion_ciudadano, name='geolocalizacion_reserva_ciudadano'),
      
      
+     path('geolocalizacion_reserva_recolector/<int:id_orden>/', views.geolocalizacion_recolector, name='geolocalizacion_reserva_recolector'),
+     
+     
      path('geolocalizacion_ciudadano_orden/<int:id_orden>/', views.geolocalizacion_ciudadano_orden_normal, name='geolocalizacion_ciudadano_orden'),
      
     # Resto de tus URLs
@@ -17,4 +20,6 @@ urlpatterns = [
     path('OrdenListGeolocalizar_para_ubicar_ciudadano', views.OrdenListGeolocalizar_para_ubicar_ciudadano.as_view(), name='OrdenListGeolocalizar_para_ubicar_ciudadano'),
     
     ################################################################################################################
+    
+    path('OrdenListGeolocalizar_para_ubicar_recolector', views.ReservaListGeolocalizar_recolector.as_view(), name='OrdenListGeolocalizar_para_ubicar_recolector'),
 ]
--- api/views.py
+++ api/views.py
@@ -81,7 +81,39 @@ def geolocalizacion_ciudadano_orden_normal(request, id_orden):
         data = response.json()
         return render(request, 'Api/error_api.html', {'data': data})
     
+############################################################
+def geolocalizacion_recolector(request, id_orden):
+    orden_reciclaje = Reserva_orden.objects.filter(id_orden=id_orden).first()
+
+    if orden_reciclaje is None:
+        return render(request, 'Api/error_api.html')
+
+    latitud = orden_reciclaje.latitud_posicion_recolector
+    longitud = orden_reciclaje.longitud_posicion_recolector
+
+    mapOptions = {
+        "center": {"lat": latitud, "lng": longitud},
+        "fullscreenControl": True,
+        "mapTypeControl": False,
+        "streetViewControl": False,
+        "zoom": 12,
+        "zoomControl": True,
+        "maxZoom": 20,
+        "mapId": ""
+        }
+
+    response = requests.get(f'https://maps.googleapis.com/maps/api/geocode/json?latlng={latitud},{longitud}&key=AIzaSyAeYeyldDKCcpeYVppGwYKzizbDf_HVnSo')
+
+    if response.status_code == 200:
+        data = response.json()
+        return render(request, 'Api/geolocalizacion_4.html', {'data': data, 'mapOptions': mapOptions})
+    else:
+        data = response.json()
+        return render(request, 'Api/error_api.html', {'data': data})
     
+    
+
+######################################################################
 class OrdenListGeolocalizar(ListView):
     model = Orden_reciclaje
     template_name = 'Api/orden_geolocalizar.html'
@@ -93,6 +125,21 @@ class OrdenListGeolocalizar(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['ordenes'] = Orden_reciclaje.objects.all()
+        return context
+
+####################################################################################
+
+class ReservaListGeolocalizar_recolector(ListView):
+    model = Reserva_orden
+    template_name = 'Api/orden_geolocalizar_reserva_recolector.html'
+
+    def post(self, request, *args, **kwargs):
+        orden_id = request.POST.get('orden_id')
+        return redirect('geolocalizacion_reserva_recolector', id_orden=orden_id)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['ordenes'] = Reserva_orden.objects.all()
         return context
 
 
